8b4c20aa3eb6e97d1499b48ba21bbabf35aa3397
mehannioui <118213854+mehannioui@users.noreply.github.com>
2024-03-21 08:33:50 -0400

    delete api

---
+++ src/app/dashboard/[fileid]/page.tsx
@@ -0,0 +1,33 @@
+import { db } from '@/db';
+import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/server';
+import { redirect } from 'next/navigation';
+
+interface PageProps {
+  params: {
+    fileid: string;
+  };
+}
+
+const Page = async ({ params }: PageProps) => {
+  // retrieve the file id from the URL
+  const { fileid } = params;
+
+  // fetch the file from the db
+
+  const { getUser } = getKindeServerSession();
+  const user = getUser();
+
+  if (!user || !user.id) redirect(`/auth-callback?origin=dashboard/${fileid}`);
+
+  // make database call
+  const file = await db.file.findFirst({
+    where: {
+      id: fileid,
+      userId: user.id,
+    },
+  });
+
+  return <div>{fileid}</div>;
+};
+
+export default Page;
